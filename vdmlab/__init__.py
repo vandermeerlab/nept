3934bf1e9be89792dc21a13db08ca10094658351
emirvine <emilymirvine@gmail.com>
2016-08-10 08:06:23 -0400

    Tests for consecutive and heatmaps from field detection
    Initial tests, additional tests needed for full coverage of all
    cases.

--- vdmlab/__init__.py
+++ vdmlab/__init__.py
@@ -2,7 +2,7 @@ from .utils import find_nearest_idx, time_slice, idx_in_pos, get_sort_idx, add_s
                    find_nearest_indices
 from .maze_breakdown import expand_line, save_spike_position
 from .tuning_curves import linear_trajectory, tuning_curve, get_speed
-from .place_fields import find_fields, get_single_field, get_heatmaps
+from .place_fields import consecutive, find_fields, get_single_field, get_heatmaps
 from .lfp_filtering import detect_swr_hilbert
 from .co_occurrence import spike_counts, cooccur_probabilities
 from .decoding import bayesian_prob, decode_location, decoded_sequences
